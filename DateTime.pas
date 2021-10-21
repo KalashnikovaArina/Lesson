00e801cc03bd2afc3a778280d5e22c36f0c83915
Arina Kalashnikova <akalas@sfedu.ru>
2021-10-21 11:02:05 +0300

    Создали функцию на високосность

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,12 @@
 ﻿uses system;
 
+function IsLeapYear(n:integer):boolean;
+begin
+if (n mod 4 = 0) and ((n mod 100 <> 0) or (n mod 400 = 0)) then result:=true
+else
+result:=false;
+end;
+
 begin
   
 end.
